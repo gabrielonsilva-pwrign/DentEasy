0313a825ada0dbdbbd1ba7b78533b60a29b19294
Gabriel Silva <lokischw@gmail.com>
2025-01-20 15:17:03 -0300

    correção entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,26 +3,36 @@
 set -e
 
 SOURCE_DIR=/var/www/html/denteasy
+MAX_RETRIES=30
+RETRY_INTERVAL=5
 
 # Função para executar as migrações
 run_migrations() {
     echo "Running database migrations..."
-    php $SOURCE_DIR/spark migrate
+    php $SOURCE_DIR/spark migrate --force
+    if [ $? -ne 0 ]; then
+        echo "Migration failed. Check the database connection and try again."
+        exit 1
+    fi
 }
 
 # Função para executar o seeding
 run_seeding() {
     echo "Checking if seeding is necessary..."
-    # Verifica se o seeding já foi executado
     if [ ! -f "$SOURCE_DIR/.seeding_completed" ]; then
         echo "Running database seeding..."
         php $SOURCE_DIR/spark db:seed MainSeeder
-        # Cria um arquivo para marcar que o seeding foi concluído
-        touch "$SOURCE_DIR/.seeding_completed"
+        if [ $? -eq 0 ]; then
+            touch "$SOURCE_DIR/.seeding_completed"
+        else
+            echo "Seeding failed. Check the logs for more information."
+            exit 1
+        fi
     else
         echo "Seeding has already been performed. Skipping..."
     fi
 }
+
 
 # Executa as migrações
 run_migrations
@@ -31,4 +41,5 @@ run_migrations
 run_seeding
 
 # Inicia o comando passado para o script (geralmente o Apache)
+echo "Starting the main application..."
 exec "$@"
