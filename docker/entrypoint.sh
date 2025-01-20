15e9cf6f1c0ac5fad05291e28fc79ff358919bf8
Gabriel Silva <lokischw@gmail.com>
2025-01-20 16:32:48 -0300

    Correção Entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,32 @@
 
 set -e
 
+MAX_RETRIES=10
+RETRY_INTERVAL=5
+
+# Função para verificar a conexão com o banco de dados
+check_database_connection() {
+    php -r "
+    \$host = getenv('database_default_hostname');
+    \$db   = getenv('database_default_database');
+    \$user = getenv('database_default_username');
+    \$pass = getenv('database_default_password');
+    
+    for (\$i = 0; \$i < $MAX_RETRIES; \$i++) {
+        try {
+            new PDO(\"mysql:host=\$host;dbname=\$db\", \$user, \$pass);
+            echo 'Database connection successful';
+            exit(0);
+        } catch (PDOException \$e) {
+            echo \"Attempt \$i: Database connection failed: \" . \$e->getMessage() . \"\n\";
+            sleep($RETRY_INTERVAL);
+        }
+    }
+    echo 'Failed to connect to the database after multiple attempts';
+    exit(1);
+    "
+}
+
 # Função para executar as migrações
 run_migrations() {
     echo "Running database migrations..."
@@ -32,11 +58,15 @@ run_seeding() {
 # Muda para o diretório da aplicação
 cd /var/www/html/denteasy
 
+# Verifica a conexão com o banco de dados
+echo "Checking database connection..."
+check_database_connection
+
 # Executa as migrações
-run_migrations
+#run_migrations
 
 # Executa o seeding (apenas uma vez)
-run_seeding
+#run_seeding
 
 # Inicia o comando passado para o script (geralmente o Apache)
 echo "Starting the main application..."
