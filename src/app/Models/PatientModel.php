39762793ed65fbb09873ccc217d8f636ca754726
Gabriel Silva <lokischw@gmail.com>
2025-02-03 13:21:44 -0300

    Retirado e-mail de paciente como único, mantido só CPF

--- src/app/Models/PatientModel.php
+++ src/app/Models/PatientModel.php
@@ -12,7 +12,7 @@ class PatientModel extends Model
     protected $useTimestamps = true;
     protected $validationRules = [
         'name' => 'required|min_length[3]|max_length[255]',
-        'email' => 'required|valid_email|is_unique[patients.email,id,{id}]',
+        'email' => 'required|valid_email',
         'cpf' => 'required|exact_length[14]|is_unique[patients.cpf,id,{id}]',
         'gender' => 'required|in_list[Homem,Mulher,Outros]',
         'birth_date' => 'required|valid_date',
